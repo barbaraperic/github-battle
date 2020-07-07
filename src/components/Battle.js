2e813fdf5838f841fa45f09045b8569fa07a6512
Barbara Peric <b.peric10@gmail.com>
2020-07-07 10:52:28 +0200

    FIX component import

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle } from 'react-icons/fa'
 import PropTypes from 'prop-types'
-import Results from './Results'
 import { ThemeConsumer } from '../contexts/theme'
 import { Link } from 'react-router-dom'
 
